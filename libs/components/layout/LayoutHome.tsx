322c19129ac4de258b7d8ec85da47c7c15b81aa8
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-09-24 21:35:04 +0900

    feat: develop mobile version

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,3 +1,4 @@
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import Head from "next/head";
 import Footer from "../Footer";
@@ -6,31 +7,56 @@ import Top from "../Top";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
-    return (
-      <>
-        <Head>
-          <title>Nestar</title>
-        </Head>
-        <Stack id={"pc-wrap"}>
-          <Stack id={"top"}>
-            <Top />
-          </Stack>
-          <Stack className={"header-main"}>
-            <Stack className={"container"}>
-              <HeaderFilter />
+    const device = useDeviceDetect();
+
+    if (device === "mobile") {
+      return (
+        <>
+          <Head>
+            <title>Nestar</title>
+          </Head>
+          <Stack id={"mobile-wrap"}>
+            <Stack id={"top"}>
+              <Top />
+            </Stack>
+
+            <Stack id={"main"}>
+              <Component {...props} />
+            </Stack>
+
+            <Stack id={"footer"}>
+              <Footer />
             </Stack>
           </Stack>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Head>
+            <title>Nestar</title>
+          </Head>
+          <Stack id={"pc-wrap"}>
+            <Stack id={"top"}>
+              <Top />
+            </Stack>
+            <Stack className={"header-main"}>
+              <Stack className={"container"}>
+                <HeaderFilter />
+              </Stack>
+            </Stack>
 
-          <Stack id={"main"}>
-            <Component {...props} />
-          </Stack>
+            <Stack id={"main"}>
+              <Component {...props} />
+            </Stack>
 
-          <Stack id={"footer"}>
-            <Footer />
+            <Stack id={"footer"}>
+              <Footer />
+            </Stack>
           </Stack>
-        </Stack>
-      </>
-    );
+        </>
+      );
+    }
   };
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { light } from "../scss/MaterialTheme/index";
 import { useState } from "react";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
+import "../scss/mobile/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
